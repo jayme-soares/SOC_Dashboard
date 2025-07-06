c344a6a1e0f4dac1c737e0bf6257a73cd45f6b2b
jayme-soares <jayme.soaress@gmail.com>
2025-07-06 17:55:07 -0300

    ajuste no caminho correto dos arquivos

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-page1 = st.Page("1_Producao_Diaria.py")
-page2 = st.Page("2_Producao_Mensal.py")
+page1 = st.Page("pages/1_Producao_Diaria.py")
+page2 = st.Page("pages/2_Producao_Mensal.py")
 
 pg = st.navigation([page1,page2])
 pg.run()
