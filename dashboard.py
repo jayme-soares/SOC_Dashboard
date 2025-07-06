283fe8fefd439593905b18e28e7a95ec211ca3e5
jayme-soares <jayme.soaress@gmail.com>
2025-07-06 17:52:59 -0300

    coreeção de bugs

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-page1 = st.Page("Producao_Diaria.py", title="Produção Diária", icon="📊")
-page2 = st.Page("Producao_Mensal.py", title="Produção Mensal", icon="📊")
+page1 = st.Page("Producao_Diaria.py")
+page2 = st.Page("Producao_Mensal.py")
 
 pg = st.navigation([page1,page2])
 pg.run()
